066f5383367745cf67afd5ae55c92cd6d94c274a
Hien Nguyen <thnguyen7779@gmail.com>
2024-01-09 11:47:47 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project introduction #
+    It's my first CI/CD pipeline in Jenkins from GitHub: WebHook
+
+# Jenkins server #
--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline!!"}
